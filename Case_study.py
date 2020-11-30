cdea985e59ace715c727c60da1582effd1198d72
nigolred <nicolo.golinucci@gmail.com>
2020-11-30 23:03:19 +0100

    Updates

--- Case_study.py
+++ Case_study.py
@@ -6,7 +6,7 @@ Created on Sun Nov 22 17:53:35 2020
 """
 
 import pymrio
-import World_Trade_Model as wtm
+import pywtm as wtm
 
 exiobase3_path = r'C:\Users\Gollinucci\Desktop\Nicolò\Lavoro\FEEM\Databases\exiobase_3.4_iot_2011_ixi.zip'
 Agg = r'Inputs\Aggregation.xlsx'
@@ -16,14 +16,37 @@ World.calc_all()
 #%% Choose aggregations and factors
 wtm.aggregate(World, Agg)
 World.calc_all()
-GDP = World.satellite.F.loc['Value Added',:].sum().sum()/10**6
+
 #%% Building the needed matrices
 wtm.prepare(World)
 
 #%% Running the model and saving the results
-WTM_World = wtm.run(World, r'Inputs\WTM_Inputs.xlsx')
-Results = World.wtm_results
-GDP_wtm = Results['GDP'].sum().sum()
-GDP_p = (GDP_wtm-GDP)*100/GDP
+import pandas as pd
 
-# for world in world_dict.keys()
+Cases = ['Baseline',
+         'EU_CT',
+         'EU_ETS']
+
+allCases = Cases + ['Exiobase']
+Sectors = list(World.get_sectors())
+Regions = list(World.get_regions())
+Factors = list(World.satellite.F.index.get_level_values(level=0))
+GDP = pd.DataFrame(0, index=pd.MultiIndex.from_product([allCases,Regions], names=['Case','Region']), columns=Sectors)
+CO2 = pd.DataFrame(0, index=pd.MultiIndex.from_product([allCases,Regions], names=['Case','Region']), columns=Sectors)
+AllResults = {}
+
+for s in Sectors:
+    for r in Regions:
+        GDP.loc[('Exiobase',r),s] = World.satellite.F.loc['Value Added',(r,s)].values
+        CO2.loc[('Exiobase',r),s] = World.satellite.F.loc['CO2',(r,s)].values/(10**12)
+
+for c in Cases:
+    Input_file = 'Inputs/Cases/'+c+'.xlsx'
+    WTM_World = wtm.run(World, Input_file, rexp=True)
+    Results = World.wtm_results
+    AllResults[c] = Results
+    
+    for s in Sectors:
+        for r in Regions:
+            GDP.loc[(c,r),s] = Results['FU'].loc['Value Added',(r,s)].values
+            CO2.loc[(c,r),s] = Results['FU'].loc['CO2',(r,s)].values/(10**12)
--- pywtm.py
+++ pywtm.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
+        # -*- coding: utf-8 -*-
 """
 Created on Sun Nov 22 17:34:39 2020
 
-@author: Golinucci
+@author: nigolred
 """
 
 def aggregate(WIOT, agg_file):
@@ -29,7 +29,7 @@ def prepare(WIOT, print_input_file=False):
 
     # build the needed matrices
     
-    WIOT.Y_reg = WIOT.Y.groupby(level=0, axis=1).sum()
+    WIOT.Y_reg = WIOT.Y.groupby(level=0, axis=1, sort=False).sum()
     WIOT.Y_wtm = pd.DataFrame(0, index=WIOT.Y_reg.index, columns=WIOT.Y_reg.columns)
     
     for i in reg:
@@ -52,26 +52,27 @@ def prepare(WIOT, print_input_file=False):
     
     if print_input_file:
         f_reg = pd.DataFrame(999999999, index=WIOT.satellite.S.index, columns=reg)
-        p_glo = pd.DataFrame(0, index=WIOT.satellite.S.index, columns=['Price of factors'])
+        p_glo = pd.DataFrame(0, index=WIOT.satellite.S.index, columns=reg)
         with pd.ExcelWriter(r'Inputs/WTM_Inputs.xlsx', mode='w') as writer:
             f_reg.to_excel(writer, sheet_name = 'Regional Endowments')
-            p_glo.to_excel(writer, sheet_name = 'Global Price of factors')
-            
+            p_glo.to_excel(writer, sheet_name = 'Regional Price of factors')
 
-def run(WIOT, Constraints):
+def run(WIOT, Constraints, rexp=False, verbose=False):
     import cvxpy as cv
     import numpy as np
     import pandas as pd
+    import pymrio
     
     # importing constraint file
     
-    pi = pd.read_excel(Constraints, sheet_name='Global Price of factors', index_col = [0,1])
+    pi = pd.read_excel(Constraints, sheet_name='Regional Price of factors', index_col = [0,1]).unstack([0,1]).to_frame()
     F_reg = pd.read_excel(Constraints, sheet_name='Regional Endowments', index_col = [0,1]).unstack([0,1]).to_frame()
     
     WIOT.pi = pi
     WIOT.F_reg = F_reg
     
     # get number of sectors and regions
+    takeall = slice(None)
     reg = list(WIOT.get_regions())
     sec = list(WIOT.get_sectors())
     r = len(reg)
@@ -88,25 +89,77 @@ def run(WIOT, Constraints):
     
     IM = cv.reshape(IM,(r*s, 1))
     
-    ObjFun = cv.matmul(cv.matmul(pi.T,WIOT.satellite.S), X) #Minimization of global factor cost
+    ObjFun = cv.matmul(cv.matmul(pi.T,WIOT.satellite.F_wtm), X) #Minimization of global factor cost
     objective = cv.Minimize(ObjFun)
     
     constraints = []
     constraints.append(X >= cv.matmul(WIOT.A_wtm, X) - IM + EX + np.sum(WIOT.Y_wtm.values, 1, keepdims=True))
     constraints.append(cv.matmul(WIOT.satellite.F_wtm, X) <= F_reg)
+    if rexp==False:
+        constraints.append(EX <= X) # I need to avoid re-export
                    
     prob = cv.Problem(objective, constraints)
-    WIOT.WTM_result = prob.solve(solver=cv.GUROBI, verbose=True)
+    WIOT.WTM_result = prob.solve(solver=cv.GUROBI, verbose=verbose)
+    
+    X_s = pd.DataFrame(X.value, index=WIOT.x.index, columns=['indout'])
+    EX_s = pd.DataFrame(EX.value, index=WIOT.A.index, columns=['Exports [M€]'])
+    IM_s = pd.DataFrame(IM.value, index=WIOT.A.index, columns=['Imports [M€]'])
+    T_s = pd.DataFrame(T.value, index=WIOT.A.index, columns=reg)
+    p_s = pd.DataFrame(constraints[0].dual_value, index=WIOT.x.index, columns=['Prices [M€/M€]'])
+    r_s = pd.DataFrame(constraints[1].dual_value, index=WIOT.F_reg.index, columns=['Scarcity Rents [M€/unit]'])
+    
+    
+    S = pd.DataFrame(np.zeros([s*r,s*r]), index=WIOT.A.index, columns=WIOT.A.columns)
+    I = pd.DataFrame(np.identity(s*r), index=WIOT.A.index, columns=WIOT.A.columns)
+    Yd = pd.DataFrame(np.diagflat(WIOT.Y_wtm.sum(axis=1).values), index=WIOT.Y_wtm.sum(axis=1).index, columns=WIOT.Y_wtm.sum(axis=1).index)
+    
+    
+    for i in reg:
+        for j in reg:
+            if i==j:
+                for k in sec:
+                    S.loc[(i,k),(j,k)] = (X_s.loc[(j,k)] / (X_s.loc[(j,k),:] + T_s.loc[(takeall,k),j].sum())).values
+            else:
+                for k in sec:
+                    S.loc[(i,k),(j,k)] = (T_s.loc[(i,k),j] / (X_s.loc[(j,k),:] + T_s.loc[(takeall,k),j].sum())).values
+    
+    A_B = S @ I @ WIOT.A_wtm
+    Y_B = S @ I @ Yd
+    Y_OD = Y_B.copy()
+    pd_s = pd.DataFrame(np.diagflat(p_s.values), index=p_s.index, columns=p_s.index)
+    Xd_s = pd.DataFrame(np.diagflat(X_s.values), index=X_s.index, columns=X_s.index)
+    VA_s = pi.add(r_s.values).T @ WIOT.satellite.F_wtm.values @ Xd_s.values
+    VA_s.index = ['M.EUR']
+    VA_s.columns = WIOT.A.columns
+    Factor_Use = pymrio.calc_F(WIOT.satellite.S,X_s)
+    FU_reg = Factor_Use.groupby(level=0, axis=1, sort=False).sum()
+    pFU_reg = Factor_Use.groupby(level=0, axis=1, sort=False).sum().div(pd.read_excel(Constraints, sheet_name='Regional Endowments', index_col = [0,1]))
+    
+    for row in range(len(Y_OD)):
+        for column in range(len(Y_OD)):
+            if row == column:
+                Y_OD.iloc[row, column] = 0
     
     WIOT.wtm_results = {
         'GDP': pd.DataFrame(objective.value/10**6, index=['World'], columns=['GDP [T€]']),
-        'X': pd.DataFrame(X.value, index=WIOT.x.index, columns=['Production [M€]']),
-        'T': pd.DataFrame(T.value, index=WIOT.A.index, columns=reg),
-        'EX': pd.DataFrame(EX.value, index=WIOT.A.index, columns=['Exports [M€]']),
-        'IM': pd.DataFrame(IM.value, index=WIOT.A.index, columns=['Imports [M€]']),
-        'p': pd.DataFrame(constraints[0].dual_value, index=WIOT.x.index, columns=['Prices [M€/M€]']),
-        'r':pd.DataFrame(constraints[1].dual_value, index=WIOT.F_reg.index, columns=['Scarcity Rents [M€/unit]'])
+        'X': X_s,
+        'T': T_s,
+        'EX': EX_s,
+        'IM': IM_s,
+        'FU': Factor_Use,
+        'FU_reg': FU_reg,
+        'pFU_reg': pFU_reg,
+        'S': S,
+        'A_B': A_B,
+        'Yd': Yd,
+        'Y_B': Y_B,
+        'Y_OD': Y_OD,
+        'p': p_s,
+        'r': r_s,
+        'Z': pd_s @ A_B @ Xd_s,
+        'VA': VA_s
         }
-
-    # nic = pymrio.IO(x=X,)    
     
+    NewWorld = pymrio.IOSystem(Z=WIOT.wtm_results['Z'], Y=WIOT.wtm_results['X'].sum(axis=1)-WIOT.wtm_results['Z'].sum(axis=1), x=WIOT.wtm_results['X'])
+    
+    return NewWorld
